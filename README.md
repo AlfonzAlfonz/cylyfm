758aecb109630b51ed0d75f96a3634516820bd03
Denis Homolík <alfonz@homolik.cz>
2023-01-08 23:40:36 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CYLYFM
+## Charge your laptop you fucking moron
+
+Simple utility to help you not to forget to plug in your laptop if your battery is 1 %.
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use battery::units::Ratio;
+use battery::Manager;
+use battery::State;
+use soloud::*;
+use std::{thread, time::Duration};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let m = Manager::new()?;
+
+  let min: Ratio = Ratio::new::<battery::units::ratio::ratio>(0.02);
+
+  let sl = Soloud::default()?;
+
+  let mut speech = audio::Speech::default();
+
+  println!("{:?}", speech);
+
+  speech.set_text("charge meeee")?;
+
+  loop {
+    let mut ok = true;
+    for b in m.batteries()?.filter_map(|x| x.ok()) {
+      let state = b.state();
+      if state != State::Charging {
+        continue;
+      }
+
+      if b.energy() / b.energy_full() < min {
+        sl.play(&speech);
+        while sl.active_voice_count() > 0 {
+          std::thread::sleep(std::time::Duration::from_millis(100));
+        }
+        ok = false;
+      }
+    }
+
+    thread::sleep(Duration::from_secs(if ok { 600 } else { 1 }));
+  }
+}
