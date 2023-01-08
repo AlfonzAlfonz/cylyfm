997b8484d177bb6ecc3ceb90105d39d76018a7e5
Denis Homolík <alfonz@homolik.cz>
2023-01-09 00:21:44 +0100

    allocate sound only when needed

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
 use battery::units::Ratio;
+use battery::Battery;
 use battery::Manager;
 use battery::State;
 use soloud::*;
-use std::{thread, time::Duration};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let m = Manager::new()?;
   let min: Ratio = Ratio::new::<battery::units::ratio::ratio>(0.02);
-  
-  let sl = Soloud::default()?;
-  let mut speech = audio::Speech::default();
-  speech.set_text("charge meeee")?;
+  let m = Manager::new()?;
 
   loop {
-    let mut ok = true;
-    for b in m.batteries()?.filter_map(|x| x.ok()) {
+    for mut b in m.batteries()?.filter_map(|x| x.ok()) {
       let state = b.state();
       if state != State::Charging {
         continue;
       }
 
-      if b.energy() / b.energy_full() < min {
+      if is_bat_ok(&b, &min) {
+        break;
+      }
+
+      let sl = Soloud::default()?;
+      let mut speech = audio::Speech::default();
+      speech.set_text("charge meeee")?;
+
+      loop {
         sl.play(&speech);
         while sl.active_voice_count() > 0 {
-          std::thread::sleep(std::time::Duration::from_millis(100));
+          sleep(Duration::from_millis(100));
         }
-        ok = false;
+
+        sleep(Duration::from_secs(1));
+
+        m.refresh(&mut b).ok();
+        if is_bat_ok(&b, &min) || state == State::Charging {
+          break;
+        }
       }
     }
 
-    thread::sleep(Duration::from_secs(if ok { 600 } else { 1 }));
+    sleep(Duration::from_secs(120));
   }
+}
+
+fn is_bat_ok(b: &Battery, min: &Ratio) -> bool {
+  &(b.energy() / b.energy_full()) >= min
 }
